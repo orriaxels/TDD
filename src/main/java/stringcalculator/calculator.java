bfb7b405a8394beaf2167ac53774f0e5bb811f02
Orri Axels <orriaxels@gmail.com>
2016-10-10 23:31:35 +0000

    Added test two numbers

--- src/main/java/stringcalculator/calculator.java
+++ src/main/java/stringcalculator/calculator.java
@@ -6,9 +6,18 @@ public class Calculator {
 	{
 		if(numbers.equals(""))
 			return 0;
+		else if(numbers.contains(","))
+		{
+			String [] array = numbers.split(",");
+			return toInt(array[0]) + toInt(array[1]);
+		}
 		else
 			return 1;
 	}
 
+	private static int toInt(String numbers)
+	{
+		return Integer.parseInt(numbers);
+	}
 
 }
--- src/test/java/stringcalculator/calculatorTest.java
+++ src/test/java/stringcalculator/calculatorTest.java
@@ -16,4 +16,10 @@ public class CalculatorTest {
 	{
 		assertEquals(1, Calculator.add("1"));
 	}
+
+	@Test
+	public void testTwoStrings()
+	{
+		assertEquals(3, Calculator.add("1,2"));
+	}
 }
